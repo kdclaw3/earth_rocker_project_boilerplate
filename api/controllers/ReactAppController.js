5f51f1c26a1032428ca79d53b5a792edef64b6e4
Dee Clawson <kdclaw3@gmail.com>
2018-07-02 11:28:55 -0400

    sails/react routing
    create a "ReactApp" controler that serves the static index file. Temporarily untill someone thinks of a better solution fixes #1.

---
+++ api/controllers/ReactAppController.js
@@ -0,0 +1,17 @@
+/**
+ * ReactAppController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+  
+	find(req, res, next) {
+
+		return res.sendFile(`${process.env.PWD}/client/public/index.html`);
+
+	}
+
+};
+
