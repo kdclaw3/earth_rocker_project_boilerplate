9c131e2541e388caa8d78c37a3fb42c4b826d379
Dee Clawson <kdclaw3@gmail.com>
2018-07-12 18:43:40 -0400

    merge sample jobs

---
+++ jobs/datacleanup/index.js
@@ -0,0 +1,5 @@
+module.exports = () => {
+
+	sails.log.info('[JOB MANAGER] this is another job, Heyooo!');
+
+};
---
+++ jobs/index.js
@@ -0,0 +1,48 @@
+/* eslint-disable no-unused-vars */
+function addMinutes(date, minutes) {
+	return new Date(date.getTime() + minutes * 60000);
+}
+const hourly = 60;
+const daily = 1440;
+/* eslint-enable no-unused-vars */
+
+
+module.exports = {
+
+	/**
+	 *
+	 * @param {integer} 	interval 		//in minutes
+	 * @param {string} 		start				//time to start the job
+	 * @param {function} 	run					//function to run
+	 */
+
+	aJob: {
+		interval: 1,
+		start: new Date(), //start this immediatly and run every minute
+		run: function () {
+			//simple function here
+			sails.log.info('[JOB MANAGER] this job runs every minute.');
+		}
+	},
+
+	bJob: {
+		interval: 2,
+		start: new Date(), //start this immediatly and run every two minutes
+		run: require('./datacleanup') //import function from a file
+	},
+
+	dataCleanup: {
+		interval: 2,
+		start: addMinutes(new Date(), 100), //start this job in 100 minutes and run every two minutes
+		run: require('./datacleanup') //import function from a file
+	},
+
+	dataQuality: {
+		interval: daily,
+		start: addMinutes(new Date(), 30), //start this job once a 'daily' 30 minutes from now
+		run: () => {
+			sails.log.info('[JOB MANAGER] this job is boring and only executes once a day.');
+		}
+	}
+
+};
