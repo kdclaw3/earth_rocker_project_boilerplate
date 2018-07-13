819d1246e6bd6de260a4a1c7444f0a0861aefbad
Dee Clawson <kdclaw3@gmail.com>
2018-07-13 17:05:44 -0400

    switch to spaces

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,62 +1,62 @@
 module.exports = {
 
-	'parser': 'babel-eslint',
-	'env': {
-		es6: true,
-		node: true,
-		browser: true,
-		mocha: true
-	},
-	'parserOptions': {
-		ecmaVersion: 6,
-		sourceType: 'module',
-		allowImportExportEverywhere: false,
-		codeFrame: false,
-		ecmaFeatures: {
-			jsx: true
-		}
-	},
-	'plugins': [
-		'react'
-	],
-	'extends': [
-		'eslint:recommended',
-		'plugin:react/recommended'
-	],
-	'rules': {
-		//enable eslint rules
-		'array-bracket-spacing': ['warn', 'never'],
-		'comma-dangle': 1,
-		'comma-spacing': ['warn', { before: false, after: true }],
-		'eol-last': ['error', 'always'],
-		'eqeqeq': 'warn',
-		'indent': ['warn', 'tab'],
-		'jsx-quotes': ['warn', 'prefer-double'],
-		'key-spacing': ['warn', {beforeColon: false, afterColon: true}],
-		'no-multi-spaces': 'warn',
-		'no-trailing-spaces': 'warn',
-		'semi': ['warn', 'always'],
-		'semi-spacing': ['warn', {before: false, after: true}],
-		'semi-style': ['warn', 'last'],
-		'space-infix-ops': 'warn',
-		'quote-props': ['warn', 'consistent-as-needed', {keywords: true, numbers: true, unnecessary: true}],
-		'quotes': ['warn', 'single', {allowTemplateLiterals: true}],
-		'yoda': ['warn', 'never', { exceptRange: true }]
+  'parser': 'babel-eslint',
+  'env': {
+    es6: true,
+    node: true,
+    browser: true,
+    mocha: true
+  },
+  'parserOptions': {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    allowImportExportEverywhere: false,
+    codeFrame: false,
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  'plugins': [
+    'react'
+  ],
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended'
+  ],
+  'rules': {
+    //enable eslint rules
+    'array-bracket-spacing': ['warn', 'never'],
+    'comma-dangle': 1,
+    'comma-spacing': ['warn', { before: false, after: true }],
+    'eol-last': ['error', 'always'],
+    'eqeqeq': 'warn',
+    'indent': ['warn', 2],
+    'jsx-quotes': ['warn', 'prefer-double'],
+    'key-spacing': ['warn', {beforeColon: false, afterColon: true}],
+    'no-multi-spaces': 'warn',
+    'no-trailing-spaces': 'warn',
+    'semi': ['warn', 'always'],
+    'semi-spacing': ['warn', {before: false, after: true}],
+    'semi-style': ['warn', 'last'],
+    'space-infix-ops': 'warn',
+    'quote-props': ['warn', 'consistent-as-needed', {keywords: true, numbers: true, unnecessary: true}],
+    'quotes': ['warn', 'single', {allowTemplateLiterals: true}],
+    'yoda': ['warn', 'never', { exceptRange: true }]
 
-		//override eslint eslint:recommended
+    //override eslint eslint:recommended
 
-		//disable eslint eslint:recommended
+    //disable eslint eslint:recommended
 
-		//enable react rules
+    //enable react rules
 
-		//override react react/recommended
+    //override react react/recommended
 
-		//disable react react/recommended
+    //disable react react/recommended
 
-	},
-	'globals': {
-		sails: true,
-		User: true
-	}
+  },
+  'globals': {
+    sails: true,
+    User: true
+  }
 
 };
--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,17 @@
 module.exports = {
 
-	presets: [
-		'@babel/preset-env',
-		'@babel/preset-react'
-	],
+  presets: [
+    '@babel/preset-env',
+    '@babel/preset-react'
+  ],
 
-	plugins: [
-		'react-hot-loader/babel',
-		'@babel/plugin-proposal-class-properties',
-		'@babel/plugin-proposal-object-rest-spread',
-		['module-resolver', {
-			root: ['./client/src']
-		}]
-	]
+  plugins: [
+    'react-hot-loader/babel',
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-object-rest-spread',
+    ['module-resolver', {
+      root: ['./client/src']
+    }]
+  ]
 
 };
